d67275b6c9cda709b34797cd4aacb8e7a27e0e86
cristianprado@live.com <shmendrick2020>
2020-08-27 14:21:28 -0300

    v2.1.1

--- app/src/main/java/com/example/memorar/db/MemosDB.java
+++ app/src/main/java/com/example/memorar/db/MemosDB.java
@@ -1,4 +1,24 @@
 package com.example.memorar.db;
 
-public class MemosDB {
+
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+import com.example.memorar.model.Memo;
+
+@Database(entities = Memo.class, version = 1)
+public abstract class MemosDB extends RoomDatabase {
+    public abstract MemosDao memosDao();
+
+    private static MemosDB instance;
+
+    public static MemosDB getInstance(Context context) { 13:24
+        if(instance==null)
+            instance= Room.databaseBuilder(context,MemosDB.class, DATABASE_NAME).allowMainThreadQueries().build();
+return instance;
+
+    }
 }
